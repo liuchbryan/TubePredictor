b090e6cdec1951d15eb8d81c6096b03642820a52
Henry Cuttell <henry.cuttell@hotmail.co.uk>
2015-07-31 23:47:27 +0100

    Added basic get tube time button
    Communicates with the back end to get a default tube time and
    displays this to the user.

--- server.js
+++ server.js
@@ -15,4 +15,16 @@ io.sockets.on('connection', function(socket) {
 
   console.log('Tube predictor started...');
 
+  socket.on('getTubeTime', function(station) {
+
+    // Get tube time logic here (time in seconds)
+    var time = '180';
+
+    console.log('Sending tube time...');
+
+    var message = '<h1>Tube leaving in: ' + time + ' seconds</h1>';
+    socket.emit('newTubeTime', message);
+
+  });
+
 });
