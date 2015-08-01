381f64210531f7b0eaca64dcd01a19d314e18af1
Bradley Kemp <bjk30@cam.ac.uk>
2015-08-01 14:20:06 +0100

    Updated socket.io address

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8080');
+var socket = io.connect('tubepredictor.duckdns.org');
 var clock;
 var current_time = 1000;
 var showing_first_train = true;
@@ -48,13 +48,13 @@ socket.on('newTubeTime', function(time) {
         clock.start();
       }
     } else {
-      ++delay_count;
       if (clock.getTime() - first_train > 50 || delay_count > 8) {
         document.getElementById("notification").innerHTML = "<div class='alert alert-warning'><strong>Delay detected!</strong></div>";
       }
     }
   } else {
     showing_first_train = true;
+    document.getElementById("notification").innerHTML = "";
     if (Math.abs(first_train - current_time) > 10) {
       current_time = first_train;
       clock.stop();
@@ -85,7 +85,18 @@ socket.on('newArrivalPrediction', function(data) {
 setInterval(function() {
   socket.emit('getTubeTime', "Moorgate");
   socket.emit('getArrivalPrediction', "Moorgate");
-}, 5000);
+}, 20000);
 
 socket.emit('getTubeTime', "Moorgate");
 socket.emit('getArrivalPrediction', "Moorgate");
+
+navigator.geolocation.getCurrentPosition(getNearestStation);
+
+function getNearestStation (position) {
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+
+  socket.emit('getNearestStation', {lat:lat, long:long});
+
+  console.log("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+}
